585991bed01d933cf529950e7aee4995adaa4891
Nadia <ncampowoytuk@gmail.com>
2016-04-30 12:54:43 +0200

    bt setup

--- Arduino/mimetik/mimetik.ino
+++ Arduino/mimetik/mimetik.ino
@@ -2,12 +2,25 @@
 // Bluetooth Communication and state implementation //
 //////////////////////////////////////////////////////
 
+#include <SoftwareSerial.h>
+// BT connection via digital pins 10 (RX) and 11 (TX).
+SoftwareSerial BT(10,11);
+
 void setup() {
-  // put your setup code here, to run once:
+  // BT setup
+  Serial.begin(9600);
+  BT.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  // Get BT messages
+  if(BT.available()){
+    int value = BT.read();
+    if(value == 'R'){
+      // do things
+    }
+  }
 
 }
